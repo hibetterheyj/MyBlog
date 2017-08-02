46f38c6f989467e328c8a36beda217d67b90015c
neoFelhz <neofelhz@gmail.com>
2017-08-02 22:08:13 +0800

    use sw-toolbox instead of my own sw.js

--- _hexo/source/sw.js
+++ _hexo/source/sw.js
@@ -1,173 +1,113 @@
-'use strict';
-const version = 'v201707312306';
-const __DEVELOPMENT__ = false;
-const __DEBUG__ = false;
-const offlineResources = [
-  '/',
-  '/static/offline.html',
-  '/static/offline.svg',
-  'https://m-nfz.b0.upaiyun.com/img/avatar-m.png',
-  'https://m-nfz.b0.upaiyun.com/favicon.png',
-  'https://m-nfz.b0.upaiyun.com/img/upyun_logo.svg'
-];
+"use strict"; (function() {
+    var cacheVersion = "-170801";
+    var staticImageCacheName = "static-image" + cacheVersion;
+    var staticAssetsCacheName = "static-assets" + cacheVersion;
+    var contentCacheName = "content" + cacheVersion;
+    var maxEntries = 100;
+    self.importScripts("https://m-nfz.b0.upaiyun.com/static/js/sw-toolbox.js");
+    self.toolbox.options.debug = false;
+    self.toolbox.options.networkTimeoutSeconds = 2;
+    /* staticImageCache */
+    self.toolbox.router.get("/img/(.*)", self.toolbox.cacheFirst, {
+        origin: /m-nfz\.b0\.upaiyun\.com/,
+        cache: {
+            name: staticImageCacheName,
+            maxEntries: maxEntries
+        }
+    });
+    self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+        origin: /p0\.ssl\.qhmsg\.com/,
+        cache: {
+            name: staticImageCacheName,
+            maxEntries: maxEntries
+        }
+    });
+        self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+        origin: /img-nfz\.b0\.upaiyun\.com/,
+        cache: {
+            name: staticImageCacheName,
+            maxEntries: maxEntries
+        }
+    });
+    self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+        origin: /ooo\.0o0\.ooo/,
+        cache: {
+            name: staticImageCacheName,
+            maxEntries: maxEntries
+        }
+    });
+    self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+        origin: /i\.loli\.net/,
+        cache: {
+            name: staticImageCacheName,
+            maxEntries: maxEntries
+        }
+    });
 
-const matchFetch = [/https?:\/\/blog.nfz.moe\//];
-const ignoreFetch = [
-  'https://blog.nfz.moe/ping.php',
-];
+    /* StaticAssetsCache */    
+    self.toolbox.router.get("/css/(.*)", self.toolbox.networkOnly, {
+        origin: /m-nfz\.b0\.upaiyun\.com/,
+    });
+    self.toolbox.router.get("/js/(.*)", self.toolbox.networkOnly, {
+        origin: /m-nfz\.b0\.upaiyun\.com/,
+    });
+    self.toolbox.router.get("/static/(.*)", self.toolbox.networkFirst, {
+        origin: /m-nfz\.b0\.upaiyun\.com/,
+        cache: {
+            name: staticAssetsCacheName,
+            maxEntries: maxEntries
+        }
+    });
+    self.toolbox.router.get("/fonts/(.*)", self.toolbox.cacheFirst, {
+        origin: /m-nfz\.b0\.upaiyun\.com/,
+        cache: {
+            name: staticAssetsCacheName,
+            maxEntries: maxEntries
+        }
+    });
 
-//////////
-// Install
-//////////
-function onInstall(event) {
-  log('install event in progress.');
-  self.skipWaiting();
-  event.waitUntil(updateStaticCache());
-}
+    /* ContentCache */ 
+    self.toolbox.router.get("/archives/(.*).html(.*)", self.toolbox.fastest, {
+        cache: {
+            name: contentCacheName,
+            maxEntries: maxEntries
+        }
+    });
+    self.toolbox.router.get("/(.*).php(.*)", self.toolbox.networkOnly, {
+    });
+    self.toolbox.router.get("/(tags|about|gallery|archives|links)(.*)", self.toolbox.networkFirst, {
+        cache: {
+            name: contentCacheName,
+            maxEntries: maxEntries
+        }
+    });
+    self.toolbox.router.get("/$", self.toolbox.networkFirst, {
+        cache: {
+            name: contentCacheName,
+            maxEntries: maxEntries
+        }
+    });
+    self.toolbox.router.get("/\?(.*)$", self.toolbox.networkFirst, {
+        cache: {
+            name: contentCacheName,
+            maxEntries: maxEntries
+        }
+    });
+    self.toolbox.router.get("/", self.toolbox.networkFirst, {
+        cache: {
+            name: contentCacheName,
+            maxEntries: maxEntries
+        }
+    });
 
-function updateStaticCache() {
-  return caches
-    .open(cacheKey('offline'))
-    .then((cache) => {
-      return cache.addAll(offlineResources);
+    self.toolbox.precache(['https://m-nfz.b0.upaiyun.com/img/avatar-m.png', 'https://m-nfz.b0.upaiyun.com/img/upyun_logo.svg','https://m-nfz.b0.upaiyun.com/fonts/MaterialIcons-Regular.woff2']);
+
+    self.addEventListener("install",
+    function(event) {
+        return event.waitUntil(self.skipWaiting())
+    });
+    self.addEventListener("activate",
+    function(event) {
+        return event.waitUntil(self.clients.claim())
     })
-    .then(() => {
-      log('installation complete!');
-    });
-}
-
-////////
-// Fetch
-////////
-function onFetch(event) {
-  const request = event.request;
-
-  if (shouldAlwaysFetch(request)) {
-    event.respondWith(networkedOrOffline(request));
-    return;
-  }
-
-  if (shouldFetchAndCache(request)) {
-    event.respondWith(networkedOrCached(request));
-    return;
-  }
-
-  event.respondWith(cachedOrNetworked(request));
-}
-
-function networkedOrCached(request) {
-  return networkedAndCache(request)
-    .catch(() => { return cachedOrOffline(request) });
-}
-
-// Stash response in cache as side-effect of network request
-function networkedAndCache(request) {
-  return fetch(request)
-    .then((response) => {
-      var copy = response.clone();
-      caches.open(cacheKey('resources'))
-        .then((cache) => {
-          cache.put(request, copy);
-        });
-
-      log("(network: cache write)", request.method, request.url);
-      return response;
-    });
-}
-
-function cachedOrNetworked(request) {
-  return caches.match(request)
-    .then((response) => {
-      log(response ? '(cached)' : '(network: cache miss)', request.method, request.url);
-      return response ||
-        networkedAndCache(request)
-          .catch(() => { return offlineResponse(request) });
-    });
-}
-
-function networkedOrOffline(request) {
-  return fetch(request)
-    .then((response) => {
-      log('(network)', request.method, request.url);
-      return response;
-    })
-    .catch(() => {
-      return offlineResponse(request);
-    });
-}
-
-function cachedOrOffline(request) {
-  return caches
-    .match(request)
-    .then((response) => {
-      return response || offlineResponse(request);
-    });
-}
-
-function offlineResponse(request) {
-  log('(offline)', request.method, request.url);
-  if (request.url.match(/\.(jpg|png|gif|svg|jpeg)(\?.*)?$/)) {
-    return caches.match('/offline.svg');
-  } else {
-    return caches.match('/offline.html');
-  }
-}
-
-///////////
-// Activate
-///////////
-function onActivate(event) {
-  log('activate event in progress.');
-  event.waitUntil(removeOldCache());
-  event.waitUntil(self.clients.claim());
-}
-
-function removeOldCache() {
-  return caches
-    .keys()
-    .then((keys) => {
-      return Promise.all( // We return a promise that settles when all outdated caches are deleted.
-        keys
-          .filter((key) => {
-            return !key.startsWith(version); // Filter by keys that don't start with the latest version prefix.
-          })
-          .map((key) => {
-            return caches.delete(key); // Return a promise that's fulfilled when each outdated cache is deleted.
-          })
-      );
-    })
-    .then(() => {
-      log('removeOldCache completed.');
-    });
-}
-
-function cacheKey() {
-  return [version, ...arguments].join(':');
-}
-
-function log() {
-  if (developmentMode()) {
-    console.log("SW:", ...arguments);
-  }
-}
-
-function shouldAlwaysFetch(request) {
-  return __DEVELOPMENT__ ||
-    request.method !== 'GET' ||
-    !matchFetch.some(regex => request.url.match(regex))
-    || ignoreFetch.some(regex => request.url.match(regex));
-}
-
-function shouldFetchAndCache(request) {
-  return ~request.headers.get('Accept').indexOf('text/html');
-}
-
-function developmentMode() { return __DEVELOPMENT__ || __DEBUG__; }
-
-log("Hello from ServiceWorker land!", version);
-
-self.addEventListener('install', onInstall);
-
-self.addEventListener('fetch', onFetch);
-
-self.addEventListener("activate", onActivate);
+})();
